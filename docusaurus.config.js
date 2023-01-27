26b2203570a1ae1958bb1225a847d3a07433fbcb
Salman <farissalmannbr@gmail.com>
2023-01-28 01:18:15 +0530

    added social link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -87,45 +87,45 @@ const config = {
       footer: {
         style: 'dark',
         links: [
-          // {
-          //   title: 'Docs',
-          //   items: [
-          //     {
-          //       label: 'Tutorial',
-          //       to: '/docs/intro',
-          //     },
-          //   ],
-          // },
-          // {
-          //   title: 'Community',
-          //   items: [
-          //     {
-          //       label: 'Stack Overflow',
-          //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-          //     },
-          //     {
-          //       label: 'Discord',
-          //       href: 'https://discordapp.com/invite/docusaurus',
-          //     },
-          //     {
-          //       label: 'Twitter',
-          //       href: 'https://twitter.com/docusaurus',
-          //     },
-          //   ],
-          // },
-          // {
-          //   title: 'More',
-          //   items: [
-          //     {
-          //       label: 'Blog',
-          //       to: '/blog',
-          //     },
-          //     {
-          //       label: 'GitHub',
-          //       href: 'https://github.com/facebook/docusaurus',
-          //     },
-          //   ],
-          // },
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'IoT',
+                to: '/category/iot-basic-workshop-',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Forum',
+                href: 'https://makergram.com/community/',
+              },
+              // {
+              //   label: 'Blog',
+              //   href: 'https://makergram.com/blog/',
+              // },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/Maker_Gram',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: 'https://makergram.com/blog/',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/MakerGram/',
+              },
+            ],
+          },
         ],
         copyright: `Copyright © ${new Date().getFullYear()} MakerGram, Inc. Built with Docusaurus.`,
       },
